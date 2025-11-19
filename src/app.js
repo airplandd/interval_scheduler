c8e6c6572e2fec4e3c58bd049617d91b5292a5a3
dane <daniel.plingau25@gmail.com>
2025-11-19 18:16:57 +0000

    Make it work with sequelize

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const homepageController = require("./controllers/homepageController");
-const topicController = require("./controllers/topicController")
+const topicController = require("./controllers/topicController");
+const bodyParser = require("body-parser");
+const sequelize = require("./database");
+
 const PORT = 3000
 
 const app = express()
 
 app.set("view engine", "ejs")
 app.set("views", "./src/views")
+app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
 
 const router = express.Router()
@@ -16,7 +20,8 @@ router.get("/topic_form", topicController.getTopicForm)
 router.post("/topics", topicController.createTopic)
 
 app.use(router)
-
-app.listen(PORT, () => {
-  console.log("Listening on port 3000")
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log("Listening on port 3000")
+  })
 })
