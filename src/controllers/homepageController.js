861801057ba32efaaad5737cfa0c41f45a07a7bd
dane <daniel.plingau25@gmail.com>
2025-11-19 18:17:50 +0000

    Get the topics from the database which are for today

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -1,7 +1,22 @@
-const getHomepage = (req, res) => {
-  const data = {
-    title: "Homepage"
+const topicModel = require("../models/topicModel")
+
+const getHomepage = async (req, res) => {
+  let data = {
+    title: "Homepage",
+    rows: []
   }
+
+  const local = new Date()
+  const date = `${local.getDate()}-${local.getMonth()}-${local.getFullYear()}`
+  await topicModel.findAll({
+    where: {
+      date: date,
+    }
+  }).then((value) => {
+    data.rows = value
+  })
+
+  console.log(data)
 
   res.render("index", data)
 }
