5985f72c0818e26de4b7e660feace2cf270d3fc3
dane <daniel.plingau25@gmail.com>
2025-11-19 19:36:08 +0000

    Add the assets endpoint and add topic page for each topic

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const homepageController = require("./controllers/homepageController");
 const topicController = require("./controllers/topicController");
 const bodyParser = require("body-parser");
 const sequelize = require("./database");
+const path = require("node:path")
 
 const PORT = 3000
 
@@ -12,12 +13,14 @@ app.set("view engine", "ejs")
 app.set("views", "./src/views")
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "assets")))
 
 const router = express.Router()
 
 router.get("/", homepageController.getHomepage);
 router.get("/topic_form", topicController.getTopicForm)
 router.post("/topics", topicController.createTopic)
+router.get("/topics/:topicid", topicController.getTopicPage)
 
 app.use(router)
 sequelize.sync().then(() => {
