e4bb30fbc628ef294c49608ab83e3da63896b53a
dane <daniel.plingau25@gmail.com>
2025-11-20 08:52:24 +0000

    Make the logic for scheduling a new topic

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -11,14 +11,69 @@ const getTopicForm = (req, res) => {
 const createTopic = (req, res) => {
   const topicName = req.body.topic_name
   const local = new Date();
-  const date = `${local.getDate()}-${local.getMonth()}-${local.getFullYear()}`
+  const date = `${local.getDate()}-${local.getMonth() + 1}-${local.getFullYear()}`
   topicModel.create({ name: topicName, date, level: 1 })
   res.redirect("/")
 
 }
 
+const getTopicPage = async (req, res) => {
+  const id = req.params.topicid
+  const data = {
+    title: "Topic" + id,
+    topicid: id
+  }
+  res.render("topic_page", data)
+}
+
+const scheduleNewTopic = async (req, res) => {
+  const id = req.params.topicid
+  const quality = req.body.quality
+  await topicModel.findByPk(id).then((value) => {
+    switch (quality) {
+      case "perfect":
+        createNewTopicBasedOnLevel(3, value)
+        break;
+
+      case "good":
+        createNewTopicBasedOnLevel(Math.round(1.5), value)
+        break;
+
+      case "bad":
+        const newDate = addDate(value.date, 1)
+        const level = 1
+        topicModel.create({
+          name: value.name,
+          date: `${newDate.getDate()}-${newDate.getMonth() + 1}-${newDate.getFullYear()}`,
+          level
+        })
+        break;
+    }
+  })
+  res.redirect("/")
+}
+
+const addDate = (topicDate, numofdays) => {
+  const dataArr = topicDate.split("-").map((el) => Number(el))
+  const date = new Date(dataArr[2], dataArr[1] - 1, dataArr[0])
+
+  date.setDate(date.getDate() + numofdays)
+  return date
+}
+
+const createNewTopicBasedOnLevel = (power, value) => {
+  const newDate = addDate(value.date, Math.pow(power, value.level))
+  const level = value.level + 1 >= 7 ? 7 : value.level + 1
+  topicModel.create({
+    name: value.name,
+    date: `${newDate.getDate()}-${newDate.getMonth() + 1}-${newDate.getFullYear()}`,
+    level
+  })
+}
 
 module.exports = {
   getTopicForm,
-  createTopic
+  createTopic,
+  getTopicPage,
+  scheduleNewTopic
 }
